4636f758ab8a34e65e39ca19a1a7bbeb17eeda60
XiaoGunFr <davidyu198000507@gmail.com>
2023-01-06 20:09:37 +0100

    changement pour revenir sur "src"

--- src/main/java/gui/carcassonne/AffGestionTuile.java
+++ src/main/java/gui/carcassonne/AffGestionTuile.java
@@ -122,7 +122,7 @@ public class AffGestionTuile extends JFrame {
     /* Affichage de la prochaine tuile dans cette fenêtre. */
     public void nextTuile(CarteComplet s, int rot) {
         carte = s;
-        String m = "Projet_POOIG/src/main/resources/modeleCarte/" + s.getCarte().toString() + rot + ".png";
+        String m = "src/main/resources/modeleCarte/" + s.getCarte().toString() + rot + ".png";
         try {
             BufferedImage fichier = ImageIO.read(new File(m));
             fichier = aff.resizeImage(fichier, 250, 250);
